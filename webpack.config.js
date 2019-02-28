a597ecb398b7043854443127b17bc27f32f307f6
Goce Dokoski <goce.dokoski@quipu.de>
2019-02-28 12:02:48 +0100

    use -env.production

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = (env, argv) => ({
   entry: './src/common/index.jsx',
   devServer: { historyApiFallback: true },
+  mode: env.production ? 'production' : 'development',
   module: {
     rules: [
       {
