50ac48ce57d0a2c3e15f23b6ac129070dca75ee3
Goce Dokoski <goce.dokoski@quipu.de>
2019-03-03 00:08:32 +0100

    add variables from webpack define plugin into jest globals

--- __tests__/setup/setupEnzyme.js
+++ __tests__/setup/setupEnzyme.js
@@ -2,11 +2,3 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() })
-
-const config = require('../../config.json')
-
-// Define paramters separately for jest, 
-// as their values are not filled in with Webpack Define plugin
-
-var WP_CONF_REMOTE_DB_URL = config.remote_db_url
-var WP_CONF_BASE_URL = "/"
